267bc86b3b9d0dcd40d7696a57c7cc9a04ea5233
Ferhat Yilmaz <ferhatyilmaz.me@pm.me>
2023-08-29 22:11:08 +0300

    feat: Implement Exercise 1-7

---
+++ ch-1/e1_7/e1_7.c
@@ -0,0 +1,17 @@
+// Author: Ferhat Yilmaz
+// 
+// Compiler: Apple clang version 14.0.3 
+//
+// Exercise 1-7
+// ------------
+// Write a program to print the value of EOF.
+
+#include <stdio.h>
+
+int main(void)
+{
+    // EOF is a constant integer defined in <stdio.h>
+    printf("Value of the EOF is %d\n", EOF);
+    
+    return 0;
+}
