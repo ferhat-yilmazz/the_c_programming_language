9be4b756bbac4d06889195787479084a88f65cfd
Ferhat Yilmaz <ferhatyilmaz.me@pm.me>
2023-08-30 23:17:15 +0300

    feat: Implement Exercise 1-8

---
+++ ch-1/e1_8/e1_8.c
@@ -0,0 +1,28 @@
+// Author: Ferhat Yilmaz
+// 
+// Compiler: Apple clang version 14.0.3 
+//
+// Exercise 1-8
+// ------------
+// Write a program to count blanks, tabs, and newlines.
+//
+// (EOF signal can be inserted by using <CRTL+D> in the Unix-like systems.)
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Decleration and definition of a variable to hold count of white-spaces
+    int ws_counts = 0;
+    int ch;
+
+    while ((ch = getchar()) != EOF)
+        if ((ch == ' ') || (ch == '\t') || (ch == '\n'))
+            ws_counts++;
+
+    // Print result when EOF inserted
+    printf("\n\n>> %d\n", ws_counts);
+
+    return 0;
+}
+
