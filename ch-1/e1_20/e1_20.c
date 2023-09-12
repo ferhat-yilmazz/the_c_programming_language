7941a5207b11c8228b85c1a17fc9cf6cbd1cbb80
Ferhat Yilmaz <ferhatyilmaz.me@pm.me>
2023-09-12 23:34:20 +0300

    fix: Fix Incorrect Behavior for Some Cases
    - Fix failure for the case "abcd\tabcd" where tabsize is 4. In that case
      response of the program should be "abcd    abcd". However, it was
      "abcdabcd". Change the while loop between do-while loop.
    
    - Add info message for EOF character at start of the program.

--- ch-1/e1_20/e1_20.c
+++ ch-1/e1_20/e1_20.c
@@ -13,6 +13,9 @@
 
 #include <stdio.h>
 
+#define MSG_INFO    ">> Info: EOF signal can be inserted by"\
+                    "using <CRTL+D>\n   in the Unix-like systems.\n\n"\
+                    ">> Please send EOF signal to termianate the program.\n\n"
 #define N 4 // Tab stop size
 
 int main(void)
@@ -20,17 +23,19 @@ int main(void)
     char ch;
     int pos = 1;
 
+    printf(MSG_INFO);
+
     // There is a calculation for tab stop point. Mod of the cursor position
     // according to tab stop size shall be 1. When tab inserted, until reach
     // this result a space character shall be added.
     // For each new line cursor position must be reset.
     while ((ch = getchar()) != EOF)
         if (ch == '\t')
-            while (pos % N != 1)
+            do
             {
                 putchar(' ');
                 pos++;
-            }
+            } while (pos % N != 1);
         else if (ch == '\n')
         {
             putchar(ch);
